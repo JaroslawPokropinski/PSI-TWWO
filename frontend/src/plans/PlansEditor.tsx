3c11fe1e8dd2bddb8942de81e88c8f38596a71c7
MatCoder77 <matkimzag@wp.pl>
2020-11-12 02:09:48 +0100

    Initial Plans pages

--- frontend/src/plans/PlansEditor.tsx
+++ frontend/src/plans/PlansEditor.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import EditorView from '../shared/EditorView';
+import useQueryParam from '../shared/useQueryParam';
+
+import './PlansEditor.css';
 
 function PlansEditor(): JSX.Element {
+  const history = useHistory();
+  const [code] = useQueryParam('code');
+  const { state } = useParams<{ state: string }>();
+  const modify = useMemo(() => state === 'create' || state === 'edit', [state]);
+  const onFinish = useCallback(
+    (/* results */) => {
+      // history.goBack();
+    },
+    [
+      /* history */
+    ]
+  );
+
   return (
     <EditorView
-      name="plans"
-      initialVals={{}}
-      onFinish={() => null}
-      queryParams=""
+      name="programs"
+      initialVals={{
+        code,
+      }}
+      onFinish={onFinish}
+      queryParams={`?code=${code}`}
       header="Edycja planów uczenia"
     />
   );
