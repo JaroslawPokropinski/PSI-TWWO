ccb3ce95be270a66c0be822b40430c707454647c
Jarosław Pokropiński <34548796+JaroslawPokropinski@users.noreply.github.com>
2020-12-30 15:28:03 +0100

    Languages (#2)
    * Create main views and add color scheme
    
    * Added views
    
    * Update views
    
    * Update programs view
    
    * Load config front config from environment
    
    * Changes in effects view
    
    * Initial Plans pages
    
    * Improve modifying key values
    
    * Add verification of cards
    
    * improv programs and cards
    
    * improv ProgramDisciplines and ProgramBlocks
    
    * Added showing object history
    
    * Fix views to work with history
    
    * Improved effects list
    
    * Improved cards list, added subject codes
    
    * Added literature for cards
    
    * Improvement of list item for plans and programmes
    
    * Add history to more views and fix x scrolling
    
    * View for plans
    
    * Restored ProgramsEditor
    
    * Minor change in mock data
    
    * Language support
    
    Co-authored-by: MatCoder77 <matkimzag@wp.pl>
    Co-authored-by: Jakub Geroń <190729@student.pwr.edu.pl>

--- frontend/src/context/App.tsx
+++ frontend/src/context/App.tsx
@@ -18,48 +18,51 @@ import Cards from '../cards/Cards';
 import CardsEditor from '../cards/CardsEditor';
 import PlanView from '../plans/PlanView';
 import Plans from '../plans/Plans';
+import LangContainer from './LangContext';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <Router basename={process.env.PUBLIC_URL}>
-        <AuthContext.Provider value={{ token: null }}>
-          <Switch>
-            <Route path="/plans/:state">
-              <PlanView />
-            </Route>
-            <Route path="/plans">
-              <Plans />
-            </Route>
-            <Route path="/cards/:state">
-              <CardsEditor />
-            </Route>
-            <Route path="/cards">
-              <Cards />
-            </Route>
-            <Route path="/programs/:state">
-              <ProgramsEditor />
-            </Route>
-            <Route path="/programs">
-              <Programs />
-            </Route>
-            <Route path="/effects/:state">
-              <EffectsEditor />
-            </Route>
-            <Route path="/effects">
-              <Effects />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/home">
-              <Home />
-            </Route>
-            <Route path="/">
-              <Redirect to="/login" />
-            </Route>
-          </Switch>
-        </AuthContext.Provider>
+        <LangContainer>
+          <AuthContext.Provider value={{ token: null }}>
+            <Switch>
+              <Route path="/plans/:state">
+                <PlanView />
+              </Route>
+              <Route path="/plans">
+                <Plans />
+              </Route>
+              <Route path="/cards/:state">
+                <CardsEditor />
+              </Route>
+              <Route path="/cards">
+                <Cards />
+              </Route>
+              <Route path="/programs/:state">
+                <ProgramsEditor />
+              </Route>
+              <Route path="/programs">
+                <Programs />
+              </Route>
+              <Route path="/effects/:state">
+                <EffectsEditor />
+              </Route>
+              <Route path="/effects">
+                <Effects />
+              </Route>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/home">
+                <Home />
+              </Route>
+              <Route path="/">
+                <Redirect to="/login" />
+              </Route>
+            </Switch>
+          </AuthContext.Provider>
+        </LangContainer>
       </Router>
     </div>
   );
---
+++ frontend/src/shared/locales.ts
@@ -0,0 +1,5 @@
+import pl from '../resources/translations/pl.json';
+import en from '../resources/translations/en.json';
+
+const locales: Record<string, Record<string, string>> = { pl, en };
+export default locales;
