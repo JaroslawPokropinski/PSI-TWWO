dd5ea9309456576bccce5b10ddf74243d7068cc8
MatCoder77 <matkimzag@wp.pl>
2021-01-27 14:39:18 +0100

    Added new roles

--- src/main/java/psi/domain/user/entity/UserRole.java
+++ src/main/java/psi/domain/user/entity/UserRole.java
@@ -4,6 +4,8 @@ public enum UserRole {
 
     ROLE_USER,
     ROLE_MODERATOR,
+    ROLE_SUBJECT_SUPERVISOR,
+    ROLE_COMMISSION_MEMBER,
     ROLE_ADMIN
 
 }
---
+++ src/main/java/psi/infrastructure/security/annotation/HasCommissionMemberRole.java
@@ -0,0 +1,14 @@
+package psi.infrastructure.security.annotation;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasRole('ROLE_COMMISSION_MEMBER')")
+public @interface HasCommissionMemberRole {
+}
